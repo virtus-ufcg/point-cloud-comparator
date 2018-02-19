1ef1b1a63f19a89accf8e779ebb122b37a6a4f20
Fernando Felix <fernandojr.ifcg@live.com>
2018-02-19 17:08:08 -0300

    Chance include headers to use relative paths

--- include/pcl/PointCloudComparator/visua.hpp
+++ include/pcl/PointCloudComparator/visua.hpp
@@ -10,19 +10,19 @@
 
 #include <iostream>
 
-#include </home/adrian/pcl/io/include/pcl/io/pcd_io.h>
-#include </home/adrian/pcl/io/include/pcl/io/ply_io.h>
-#include </home/adrian/pcl/common/include/pcl/point_cloud.h>
-#include </home/adrian/pcl/common/include/pcl/console/parse.h>
-#include </home/adrian/pcl/common/include/pcl/common/transforms.h>
-#include </home/adrian/pcl/visualization/include/pcl/visualization/pcl_visualizer.h>
-#include </home/adrian/pcl/visualization/include/pcl/visualization/point_cloud_color_handlers.h>
+#include <~/pcl/io/include/pcl/io/pcd_io.h>
+#include <~/pcl/io/include/pcl/io/ply_io.h>
+#include <~/pcl/common/include/pcl/point_cloud.h>
+#include <~/pcl/common/include/pcl/console/parse.h>
+#include <~/pcl/common/include/pcl/common/transforms.h>
+#include <~/pcl/visualization/include/pcl/visualization/pcl_visualizer.h>
+#include <~/pcl/visualization/include/pcl/visualization/point_cloud_color_handlers.h>
 #include <boost/thread/thread.hpp>
-#include </home/adrian/pcl/common/include/pcl/common/common_headers.h>
-#include </home/adrian/pcl/features/include/pcl/features/normal_3d.h>
-#include </home/adrian/pcl/common/include/pcl/range_image/range_image.h>
-#include </home/adrian/pcl/features/include/pcl/features/range_image_border_extractor.h>
-#include </home/adrian/pcl/keypoints/include/pcl/keypoints/narf_keypoint.h>
+#include <~/pcl/common/include/pcl/common/common_headers.h>
+#include <~/pcl/features/include/pcl/features/normal_3d.h>
+#include <~/pcl/common/include/pcl/range_image/range_image.h>
+#include <~/pcl/features/include/pcl/features/range_image_border_extractor.h>
+#include <~/pcl/keypoints/include/pcl/keypoints/narf_keypoint.h>
 
 namespace pcl {
 namespace PointCloudComparator {
